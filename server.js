4f5b044c7cd3868444c04f48b9501a818f440d82
Matthew <mattsully01@gmail.com>
2019-11-06 22:30:30 -0700

    adding style changes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/app/public"));
 require('./app/public/routes/htmlRoutes')(app)
 
+
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
 
