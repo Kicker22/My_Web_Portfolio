72edd91ea6920a2d0ffbe8c9df7c4adcb1c08a8d
Matthew <mattsully01@gmail.com>
2023-07-13 10:54:06 -0600

    adding changes

--- app/public/routes/htmlRoutes.js
+++ app/public/routes/htmlRoutes.js
@@ -5,9 +5,9 @@ module.exports = function(app) {
     app.use(express.static('./public'));
 
     app.get('/', function(req,res){
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    })
-    app.get('/landing', function(req,res){
         res.sendFile(path.join(__dirname, "../html/landing.html"));
+    })
+    app.get('/about', function(req,res){
+        res.sendFile(path.join(__dirname, "../html/index.html"));
     })
 }
